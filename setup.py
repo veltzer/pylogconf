00f1c3147bab19a432589a1af156c7c34cd4b102
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-09 17:17:43 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if not sys.version_info[0] == 3:
 
 setuptools.setup(
     name='pylogconf',
-    version='0.0.3',
+    version='0.0.4',
     description='pylogconf is a logging configurator for python logging',
     long_description='pylogconf is a logging configurator for python logging',
     url='https://veltzer.github.io/pylogconf',
