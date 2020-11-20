1e0fbedf131e05f6b8d8093c05c64f6bdc7bc152
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-20 12:38:41 +0200

    dependencies do not need explanations

--- config/python.py
+++ config/python.py
@@ -9,29 +9,28 @@ setup_requires = [
 ]
 
 test_requires = [
-    "pylint",  # to check for lint errors
-    "pytest",  # for testing
-    "pytest-cov",  # for testing
-    "flake8",  # for linting
-    "pymakehelper",  # for make
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "flake8",
+    "pymakehelper",
 ]
 
 install_requires = [
-    'pyfakeuse',  # for avoiding use of some of the parameters to functions
-    'logging_tree',  # for printing the logging tree
-    'pyyaml',  # for parsing the YAML configuration file
-    # 'systemd',  # for systemd interface
+    'pyfakeuse',
+    'logging_tree',
+    'pyyaml',
+  
 ]
 
 dev_requires = [
-    'pypitools',  # for upload and registration
-    'pydmt',  # for building
-    'pyclassifiers',  # for specifying classifications
-    'scrapy',  # for developing the scrapy support
+    'pypitools',
+    'pydmt',
+    'pyclassifiers',
+    'scrapy',
 ]
 
 python_requires = ">=3.6"
 
 extras_require = {
-    # ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
 }
