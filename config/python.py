78a0830050de43a187625a325b61c306e320f288
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:53 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,9 +2,6 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-setup_requires = []
-run_requires = []
 test_requires = [
     "pylint",
     "pytest",
@@ -13,18 +10,16 @@ test_requires = [
     "pymakehelper",
 ]
 install_requires = [
-    'pyfakeuse',
-    'logging_tree',
-    'pyyaml',
+    "pyfakeuse",
+    "logging_tree",
+    "pyyaml",
 ]
 dev_requires = [
-    'pypitools',
-    'pydmt',
-    'pyclassifiers',
-    'scrapy',
+    "pypitools",
+    "pydmt",
+    "pyclassifiers",
+    "scrapy",
 ]
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
