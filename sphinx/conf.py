95048a815bbdf9b38c4dcb0b724075a357656fd3
Mark Veltzer <mark.veltzer@gmail.com>
2022-06-30 17:53:25 +0300

    mypy fixes

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -19,6 +19,8 @@
 #
 import os
 import sys
+from typing import Dict
+
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -120,7 +122,7 @@ htmlhelp_basename = 'pylogconf_doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
+latex_elements: Dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
