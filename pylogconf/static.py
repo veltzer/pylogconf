a6e89fe477f99ff8fa37b75188302bf7c63f2bfc
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:18 +0300

    more

---
+++ pylogconf/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.38"
+DESCRIPTION = "correctly configure python logging"
+APP_NAME = "pylogconf"
+LOGGER_NAME = "pylogconf"
