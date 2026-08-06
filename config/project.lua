2e7c69da9ac04471f6c6347bfb246dc7c760fd45
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,11 @@
+NAME = "pylogconf"
+DESCRIPTION_SHORT = "correctly configure python logging"
+KEYWORDS = {
+    "python",
+    "logging",
+    "configuration",
+    "easy",
+    "yaml",
+    "json",
+    "debug",
+}
